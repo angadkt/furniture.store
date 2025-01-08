f66c62ad01022269a18440732421a87f6aa87c6e
angadkt <angadktofficial@gmail.com>
2025-01-08 09:32:56 +0530

    vercel commit

--- src/user/components/CategorySection.jsx
+++ src/user/components/CategorySection.jsx
@@ -1,39 +1,13 @@
 import axios from "axios";
-import { div } from "framer-motion/client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import ImageCarousel from "./Carousel/ImageCarousel";
 
 const CategorySection = () => {
   const navigate = useNavigate();
 
-  // const [Categories, setCategories] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get("http://localhost:5999/categories");
-  //       setCategories(response.data);
-  //     } catch (err) {
-  //       console.log("error getting category");
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
   return (
-    // <div className="flex justify-center mt-20 flex-col items-center mx-16 gap-10 ">
-    //   <div className="font-bold text-4xl">categories</div>
-    //   <div className="flex flex-wrap gap-8 justify-center ">
-    //     {
-    //       Categories.map((item)=>(
-    //         <div className="  p-5 rounded-xl shadow-2xl bg-white" key={item.id} onClick={()=>navigate(item.link)}>
-    //           <img className="w-72 h-80 rounded-xl" src={item.image} alt={item.name} />
-    //           <div className="font-semibold mt-5"><center>{item.name}</center></div>
-    //         </div>
-    //       ))
-    //     }
-    //   </div>
-    // </div>
     <div className="w-full h-full mt-10">
       <div className="flex flex-col flex-wrap gap-5">
         <div className="flex flex-wrap gap-5  px-20">
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example: Split React and React-DOM into a separate chunk
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Optional: Increase the chunk size warning limit
+  },
+});
